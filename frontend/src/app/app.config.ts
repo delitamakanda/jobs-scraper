a18a55d7f48a9d7ddcbbd338b8bb32286514a51a
Délita MAKANDA <delita.makanda@axivit.com>
2026-05-13 21:39:29 +0200

    feat: enhance app configuration with zone change detection and HTTP client setup

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideZonelessChangeDetection(),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ]
 };
