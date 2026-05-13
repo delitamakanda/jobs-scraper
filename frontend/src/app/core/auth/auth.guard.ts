125bcf2dcc7433b0159787e53eb8b53523f04e5a
Délita MAKANDA <delita.makanda@axivit.com>
2026-05-13 21:42:10 +0200

    feat: implement authentication guard to redirect unauthenticated users to login

--- frontend/src/app/core/auth/auth.guard.ts
+++ frontend/src/app/core/auth/auth.guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const access_token = localStorage.getItem('access_token');
+  const router = inject(Router);
+
+  if (!access_token) {
+    router.navigate(['/auth/login']);
+    return false;
+  }
+  
   return true;
 };
