9c6a0151357c1c9effefad0c57c403b26a3418f4
Délita MAKANDA <delita.makanda@axivit.com>
2026-05-13 19:54:49 +0200

    feat: add shell and sidebar components with basic structure and styles

---
+++ frontend/src/app/core/layout/shell/shell.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shell',
+  imports: [],
+  templateUrl: './shell.component.html',
+  styleUrl: './shell.component.css',
+})
+export class ShellComponent {}
