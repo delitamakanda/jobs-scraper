d4ad7364486cdcc02a8e2a30703598bc2485625c
Délita MAKANDA <delita.makanda@axivit.com>
2026-05-13 14:36:45 +0200

    add serializers for JobOffer and JobMatch models

---
+++ apps/jobs/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from apps.jobs.models import JobMatch, JobOffer
+
+class JobOfferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobOffer
+        fields = '__all__'
+        read_only_fields = [
+            'user',
+            'required_skills',
+            'nice_to_have_skills',
+            'ai_summary',
+            'red_flag',
+            'created_at',
+            'updated_at',
+        ]
+
+
+class JobMatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobMatch
+        fields = '__all__'
+        read_only_fields = [
+            'created_at',
+        ]
