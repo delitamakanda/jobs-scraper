52f1490de47d23a8bc308d075a2d6345facb3138
Délita MAKANDA <delita.makanda@axivit.com>
2026-05-20 17:47:39 +0200

    feat: add application card, kanban, cover letter, and interview prep dialog components

---
+++ frontend/src/app/features/applications/dialogs/cover-letter/cover-letter-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cover-letter-dialogs',
+  imports: [],
+  templateUrl: './cover-letter-dialog.component.html',
+  styleUrl: './cover-letter-dialog.component.css',
+})
+export class CoverLetterDialogComponent {}
---
+++ frontend/src/app/features/applications/dialogs/interview-prep/interview-prep-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview-prep-dialog',
+  imports: [],
+  templateUrl: './interview-prep-dialog.component.html',
+  styleUrl: './interview-prep-dialog.component.css',
+})
+export class InterviewPrepDialogComponent {}
