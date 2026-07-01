d94bd9bded8359f63aec554d4e2223401d5730ed
Délita MAKANDA <delita.makanda@axivit.com>
2026-07-01 17:59:21 +0200

    feat: enhance loading service to track loading count

--- frontend/src/app/core/services/loading.service.ts
+++ frontend/src/app/core/services/loading.service.ts
@@ -5,9 +5,13 @@ import { Injectable, signal } from '@angular/core';
 })
 export class LoadingService {
   private _loading = signal(false);
+  private _count = signal(0);
+
+  readonly count = this._count.asReadonly();
   readonly loading = this._loading.asReadonly();
 
   setLoading(loading: boolean) {
     this._loading.set(loading);
+    this._count.set(loading ? this._count() + 1 : this._count() - 1);
   }
 }
